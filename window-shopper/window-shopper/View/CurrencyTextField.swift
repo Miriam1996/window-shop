0fd58d97189c9f6b859670740dda93916654737b
Miriam Schnoll <miriamschnoll1996@gmail.com>
2019-03-11 19:34:53 -0400

    added currency button to app

--- window-shopper/window-shopper/View/CurrencyTextField.swift
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 
 @IBDesignable
 class CurrencyTextField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame:CGRect(x: 5, y:(frame.size.height/2 ) - size/2, width:size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 0.8)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius  = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+        
+    }
     override func prepareForInterfaceBuilder() {
          costumizeView()
     }
